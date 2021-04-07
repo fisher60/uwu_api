cc115da4d78fe7838bc2f9815177a5345d58706c
fisher60 <wakefollower@gmail.com>
2021-04-06 22:29:35 -0700

    fix function names for proper documentation

--- api/main.py
+++ api/main.py
@@ -16,12 +16,12 @@ async def translate_to_uwu(string: str) -> dict:
 
 
 @app.get("/stutter/{string}")
-async def translate_to_uwu(string: str) -> dict:
+async def translate_to_stutter(string: str) -> dict:
     """Makes a string stutter."""
     return {"message": stutter(string)}
 
 
 @app.get("/uwu-stutter/{string}")
-async def translate_to_uwu(string: str) -> dict:
+async def translate_to_uwu_and_stutter(string: str) -> dict:
     """Adds uwu *with* a stutter."""
     return {"message": stutter(to_uwu(string))}
